7275cd577769dd7c760e9609004bc7c23f55f1f4
Sérgio SIlva <168369897+SergioAdrianoA@users.noreply.github.com>
2026-03-17 10:13:41 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,20 @@
-const CACHE_NAME="rotas-lite";
+const CACHE_NAME = "rotas-cache-v1"
 
-const FILES=[
-"./",
-"./index.html"
-];
+const urlsToCache = [
+  "./",
+  "./index.html"
+]
 
-self.addEventListener("install",event=>{
-
-event.waitUntil(
-
-caches.open(CACHE_NAME).then(cache=>{
-
-return cache.addAll(FILES);
-
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  )
 })
 
-);
-
-});
-
-
-
-self.addEventListener("fetch",event=>{
-
-event.respondWith(
-
-fetch(event.request).catch(()=>{
-
-return caches.match(event.request);
-
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  )
 })
-
-);
-
-});
